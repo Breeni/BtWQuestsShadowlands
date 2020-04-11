50d0d3e9255cf748c510d92741085d25f3af4eeb
Breeni <breeni@mythision.com>
2020-04-11 17:47:47 +0100

    Initial commit

---
+++ Defines.lua
@@ -0,0 +1,12 @@
+local BtWQuests = BtWQuests;
+BtWQuests.Constant.Expansions.Shadowlands = LE_EXPANSION_SHADOWLANDS or 8;
+BtWQuests.Constant.Category.Shadowlands = BtWQuests.Constant.Category.Shadowlands or {};
+BtWQuests.Constant.Chain.Shadowlands = BtWQuests.Constant.Chain.Shadowlands or {};
+local Category = BtWQuests.Constant.Category.Shadowlands;
+local Chain = BtWQuests.Constant.Chain.Shadowlands;
+
+Category.Basion = 901
+Chain.Basion = {}
+
+BtWQuests.Database:AddExpansion(BtWQuests.Constant.Expansions.Shadowlands, {
+})
