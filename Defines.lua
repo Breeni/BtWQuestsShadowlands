5870c519f33a88314e8f9ad3d7d2e9b08325cc23
Breeni <breeni@mythision.com>
2020-08-06 23:58:21 +0100

    Fixed defines

--- Defines.lua
+++ Defines.lua
@@ -1,12 +1,18 @@
-local BtWQuests = BtWQuests;
-BtWQuests.Constant.Expansions.Shadowlands = LE_EXPANSION_SHADOWLANDS or 8;
-BtWQuests.Constant.Category.Shadowlands = BtWQuests.Constant.Category.Shadowlands or {};
-BtWQuests.Constant.Chain.Shadowlands = BtWQuests.Constant.Chain.Shadowlands or {};
-local Category = BtWQuests.Constant.Category.Shadowlands;
-local Chain = BtWQuests.Constant.Chain.Shadowlands;
+local BtWQuests = BtWQuests
+BtWQuests.Constant.Expansions.Shadowlands = LE_EXPANSION_SHADOWLANDS or 8
+BtWQuests.Constant.Category.Shadowlands = BtWQuests.Constant.Category.Shadowlands or {}
+BtWQuests.Constant.Chain.Shadowlands = BtWQuests.Constant.Chain.Shadowlands or {}
+local Category = BtWQuests.Constant.Category.Shadowlands
+local Chain = BtWQuests.Constant.Chain.Shadowlands
 
-Category.Basion = 901
-Chain.Basion = {}
+Category.Bastion = 901
+Category.Maldraxxus = 902
+Category.Ardenweald = 903
+Category.Revendreth = 904
+Chain.Bastion = {}
+Chain.Maldraxxus = {}
+Chain.Ardenweald = {}
+Chain.Revendreth = {}
 
 BtWQuests.Database:AddExpansion(BtWQuests.Constant.Expansions.Shadowlands, {
 })
