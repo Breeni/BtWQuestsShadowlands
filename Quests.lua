616e4b8c6ee80864dae249a2b0e66eec24264b8b
Breeni <breeni@mythision.com>
2020-08-08 01:55:23 +0100

    Database update

--- Quests.lua
+++ Quests.lua
@@ -1,1 +1,640 @@
-
+BtWQuestsDatabase:AddQuestsTable({
+    [57025] = {
+        name = "A Plea to Revendreth",
+        contentTuningID = 746,
+        source = {
+            type = "npc",
+            id = 159478,
+            locations = {
+                [1670] = {
+                    {
+                        x = 0.389027,
+                        y = 0.699924,
+                    },
+                },
+                [1671] = {
+                    {
+                        x = 0.490134,
+                        y = 0.60049,
+                    },
+                },
+            },
+        },
+    },
+    [57787] = {
+        name = "Keep to the Path",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 170533,
+            locations = {
+                [1565] = {
+                    {
+                        x = 0.656668,
+                        y = 0.279035,
+                    },
+                },
+            },
+        },
+    },
+    [57816] = {
+        name = "Dreamweaver",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 158487,
+        },
+    },
+    [57825] = {
+        name = "Delivery for Guardian Kota",
+        contentTuningID = 807,
+    },
+    [57947] = {
+        name = "Spirits of the Glen",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 158487,
+        },
+    },
+    [57948] = {
+        name = "Nothing Left to Give",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 159638,
+        },
+    },
+    [57949] = {
+        name = "They Need to Calm Down",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 159638,
+        },
+    },
+    [57950] = {
+        name = "Mizik the Haughty",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 159639,
+        },
+    },
+    [57951] = {
+        name = "Souls Come Home",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 159638,
+        },
+    },
+    [58026] = {
+        name = "When a Gorm Eats a God",
+        contentTuningID = 807,
+    },
+    [58473] = {
+        name = "Echoes of Tirna Noch",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160963,
+        },
+    },
+    [58480] = {
+        name = "Read the Roots",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161378,
+        },
+    },
+    [58483] = {
+        name = "Mementos",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161379,
+        },
+    },
+    [58484] = {
+        name = "Take What You Can",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161378,
+        },
+    },
+    [58486] = {
+        name = "He's Drust in the Way",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161446,
+        },
+    },
+    [58488] = {
+        name = "Go for the Heart",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161492,
+        },
+    },
+    [58524] = {
+        name = "Sparkles Rain from Above",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161494,
+        },
+    },
+    [58589] = {
+        name = "The Restless Dreamer",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160962,
+        },
+    },
+    [58590] = {
+        name = "Visions of the Dreamer: Origins",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160962,
+        },
+    },
+    [58591] = {
+        name = "Despoilers",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160894,
+        },
+    },
+    [58592] = {
+        name = "Caring for the Caretakers",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160962,
+        },
+    },
+    [58593] = {
+        name = "End of the Dream",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160962,
+        },
+    },
+    [58714] = {
+        name = "The Forest Has Eyes",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161847,
+        },
+    },
+    [58719] = {
+        name = "The Droman's Call",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160894,
+        },
+    },
+    [58720] = {
+        name = "Missing!",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160894,
+        },
+    },
+    [58721] = {
+        name = "Awaken the Dreamer",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169869,
+        },
+    },
+    [58723] = {
+        name = "The Court of Winter",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 162968,
+            locations = {
+                [1565] = {
+                    {
+                        x = 0.452933,
+                        y = 0.630337,
+                    },
+                },
+            },
+        },
+    },
+    [58724] = {
+        name = "The Queen's Request",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 162968,
+        },
+    },
+    [58869] = {
+        name = "Battle for Hibernal Hollow",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169541,
+        },
+    },
+    [60338] = {
+        name = "Journey to Ardenweald",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 173383,
+            locations = {
+                [1670] = {
+                    {
+                        x = 0.402527,
+                        y = 0.690498,
+                    },
+                },
+            },
+        },
+    },
+    [60341] = {
+        name = "First on the Agenda",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 157788,
+            locations = {
+                [1565] = {
+                    {
+                        x = 0.650623,
+                        y = 0.195947,
+                    },
+                },
+            },
+        },
+    },
+    [60476] = {
+        name = "Seize the Means of Production",
+        contentTuningID = 837,
+    },
+    [60519] = {
+        name = "Audience with the Winter Queen",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 158487,
+            locations = {
+                [1565] = {
+                    {
+                        x = 0.63434,
+                        y = 0.360429,
+                    },
+                },
+            },
+        },
+    },
+    [60520] = {
+        name = "Nightmares Manifest",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169142,
+        },
+    },
+    [60521] = {
+        name = "Call of the Hunt",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169031,
+        },
+    },
+    [60522] = {
+        name = "Return to Tirna Vaal",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169935,
+        },
+    },
+    [60563] = {
+        name = "Tending to Wildseeds",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169116,
+        },
+    },
+    [60567] = {
+        name = "Shooing Wildlife",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169116,
+        },
+    },
+    [60572] = {
+        name = "For the Sake of Spirit",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160894,
+        },
+    },
+    [60575] = {
+        name = "Belly Full of Fae",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169202,
+        },
+    },
+    [60577] = {
+        name = "Hungry for Animacones",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169202,
+        },
+    },
+    [60578] = {
+        name = "Visions of the Dreamer: The Betrayal",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 160962,
+        },
+    },
+    [60594] = {
+        name = "One Special Spirit",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169217,
+        },
+    },
+    [60597] = {
+        name = "Trouble at the Gormling Corral",
+        contentTuningID = 837,
+    },
+    [60600] = {
+        name = "Preparing for the Winter Queen",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169031,
+        },
+    },
+    [60621] = {
+        name = "Enemies at the Gates",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 162541,
+        },
+    },
+    [60624] = {
+        name = "Ride to Heartwood Grove",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 158487,
+        },
+    },
+    [60628] = {
+        name = "The Missing Hunters",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169584,
+        },
+    },
+    [60629] = {
+        name = "Extreme Recycling",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169584,
+        },
+    },
+    [60630] = {
+        name = "Totem Eclipse",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169584,
+        },
+    },
+    [60631] = {
+        name = "Big Problem, Little Vorkai",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169584,
+        },
+    },
+    [60632] = {
+        name = "I Know Your Face",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169772,
+        },
+    },
+    [60637] = {
+        name = "The End of Former Friends",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169686,
+        },
+    },
+    [60638] = {
+        name = "Recovering Wildseeds",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169686,
+        },
+    },
+    [60639] = {
+        name = "Heart of the Grove",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169686,
+        },
+    },
+    [60647] = {
+        name = "Recovering the Animacones",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169748,
+        },
+    },
+    [60648] = {
+        name = "Survivors of Heartwood Grove",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169748,
+        },
+    },
+    [60649] = {
+        name = "A Thorn In Their Side",
+        contentTuningID = 837,
+    },
+    [60661] = {
+        name = "Dying Dreams",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 161847,
+        },
+    },
+    [60671] = {
+        name = "The Sacrifices We Must Make",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169868,
+        },
+    },
+    [60709] = {
+        name = "Recovering the Heart",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169956,
+        },
+    },
+    [60724] = {
+        name = "Heartless",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 170012,
+        },
+    },
+    [60738] = {
+        name = "The Way to Hibernal Hollow",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 169142,
+        },
+    },
+    [60739] = {
+        name = "Tough Crowd",
+        contentTuningID = 837,
+    },
+    [60763] = {
+        name = "I Moustache You to Lend a Hand",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 168341,
+        },
+    },
+    [60764] = {
+        name = "Soothing Song",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 170172,
+        },
+    },
+    [60778] = {
+        name = "Wildseed Rescue",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 170175,
+        },
+    },
+    [60839] = {
+        name = "Remnants of the Wild Hunt",
+        contentTuningID = 1931,
+        source = {
+            type = "npc",
+            id = 170290,
+        },
+    },
+    [60840] = {
+        name = "Wild Hunt Offensive",
+        contentTuningID = 1931,
+    },
+    [60855] = {
+        name = "Gormageddon",
+        contentTuningID = 837,
+    },
+    [60856] = {
+        name = "Toppling the Brute",
+        contentTuningID = 1931,
+        source = {
+            type = "npc",
+            id = 170485,
+        },
+    },
+    [60857] = {
+        name = "We Can't Save Them All",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 170324,
+        },
+    },
+    [60859] = {
+        name = "Souls of the Forest",
+        contentTuningID = 807,
+        source = {
+            type = "npc",
+            id = 170533,
+        },
+    },
+    [60881] = {
+        name = "Ride of the Wild Hunt",
+        contentTuningID = 1931,
+        source = {
+            type = "npc",
+            id = 170485,
+        },
+    },
+    [60901] = {
+        name = "Passage to Hibernal Hollow",
+        contentTuningID = 1931,
+        source = {
+            type = "npc",
+            id = 170290,
+        },
+    },
+    [60905] = {
+        name = "Infusing the Wildseed",
+        contentTuningID = 1931,
+        source = {
+            type = "npc",
+            id = 160894,
+        },
+    },
+    [61947] = {
+        name = "Lurking In The Shadows",
+        contentTuningID = 837,
+    },
+    [61948] = {
+        name = "Airborne Defense Force",
+        contentTuningID = 837,
+    },
+})
