e701e4892f002704308088352d9a0aa267e24803
Breeni <breeni@mythision.com>
2020-08-13 04:19:21 +0100

    Database

--- Objects.lua
+++ Objects.lua
@@ -1,1 +1,103 @@
+----- AUTO GENERATED - DO NOT EDIT
 
+BtWQuestsDatabase:AddObjectsTable({
+    [344726] = {
+        name = "Dark Sermon",
+        locations = {
+            [1533] = {
+                {
+                    x = 0.572313,
+                    y = 0.717962,
+                },
+            },
+        },
+    },
+    [348558] = {
+        name = "Notice",
+        locations = {
+            [1533] = {
+                {
+                    x = 0.515325,
+                    y = 0.471975,
+                },
+            },
+        },
+    },
+    [351467] = {
+        name = "Faded Epitaph",
+        locations = {
+            [1536] = {
+                {
+                    x = 0.483272,
+                    y = 0.577776,
+                },
+            },
+        },
+    },
+    [351885] = {
+        name = "Wanted: Aggregate of Doom",
+        locations = {
+            [1525] = {
+                {
+                    x = 0.590793,
+                    y = 0.692048,
+                },
+            },
+        },
+    },
+    [352027] = {
+        name = "Wanted Scroll",
+        locations = {
+            [1533] = {
+                {
+                    x = 0.532746,
+                    y = 0.464217,
+                },
+            },
+        },
+    },
+    [352028] = {
+        name = "Wanted Scroll",
+        locations = {
+            [1533] = {
+                {
+                    x = 0.491363,
+                    y = 0.72821,
+                },
+            },
+        },
+    },
+    [352490] = {
+        name = "Join the Rebellion!",
+        locations = {
+            [1525] = {
+                {
+                    x = 0.677427,
+                    y = 0.676637,
+                },
+            },
+        },
+    },
+    [353645] = {
+        name = "Marileth's Journal",
+        locations = {
+            [1536] = {
+                {
+                    x = 0.565349,
+                    y = 0.572116,
+                },
+            },
+        },
+    },
+    [355798] = {
+        name = "Cage",
+        locations = {
+            [1543] = {
+                {
+                    x = 0.384258,
+                    y = 0.48483,
+                },
+            },
+        },
+    },
+})
